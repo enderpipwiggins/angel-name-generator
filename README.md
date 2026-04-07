27819545a0fb10c981ff43f12923ea5d27f7bce0
Phillip Andreason <enderpipwiggins@users.noreply.github.com>
2026-04-07 15:31:23 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Angel Name Generator (Angular)
+
+This is a minimal Angular 21.2.6 app that uses `AngelNameGeneratorService` to produce angel names.
+
+Quick start:
+
+1. npm install
+2. npm start
+
+The app will open in your browser (ng serve).
---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { AngelNameGeneratorService, Tradition, Domain, SuffixFilter, GeneratedAngel } from './angel-name-generator.service';
+
+@Component({
+  standalone: false,
+  selector: 'app-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+  tradition: Tradition = 'all';
+  domain: Domain = 'any';
+  suffixFilter: SuffixFilter = 'any';
+  count = 9;
+  results: GeneratedAngel[] = [];
+
+  traditions: Tradition[] = ['hebrew','christian','islamic','enochian','all'];
+  domains: Domain[] = ['any','celestial','nature','divine','concepts','geography','time','hidden','war','healing'];
+  suffixes: SuffixFilter[] = ['el','iel','on','il','ail','eel','none','any'];
+
+  constructor(private svc: AngelNameGeneratorService) {}
+
+  generate() {
+    this.results = this.svc.generate({ tradition: this.tradition, domain: this.domain, suffixFilter: this.suffixFilter, count: this.count });
+  }
+}
